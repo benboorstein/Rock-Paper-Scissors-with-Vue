d342dac6b7a25032d29abe2e00e5c5c5a55d6452
Ben <benboorstein@gmail.com>
2021-06-04 13:30:19 -0400

    html and js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,35 @@
 const App = {
     data() {
         return {
-            dataArr: [
-                {
-                    //////////////////something
-                }
+            // rules: { 
+            //     //rock: 'scissors',
+            //     //paper: 'rock',
+            //     //scissors: 'paper'
+            // }
+            tools: [
+                'Rock',
+                'Paper',
+                'Scissors'
+            ],
+            results: [
+                { result: 'You win' },
+                { result: 'Computer wins' },
+                { result: 'Tie game' }  
             ]
         }
     },
     methods: {
-        bbbbb() {
-            //////////something
+        func(theResult) { // rename this function at some point
+            // console.log(this.results[0].result)
+            theResult = this.results[0].result
         }
     }
 }
 
 Vue.createApp(App).mount('#app')
+
+
+// old questions for self:
+// R my thought of storing the result string in "result" because I THINK this will render '{{ result }}' properly.
+// The key is figuring out how to handle the 'rules' object.
+// Also: Can I just have one of the three (RPS) <li> in the HTML by having an array of them in JS and looping through them in the HTML?
