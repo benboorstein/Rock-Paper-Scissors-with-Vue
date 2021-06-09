b8023022da9e5d9d2617332da63385db5571015a
Ben <benboorstein@gmail.com>
2021-06-09 19:36:50 -0400

    html, css, js changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,44 @@
 const App = {
     data() {
         return {
-            // rules: { 
-            //     //rock: 'scissors',
-            //     //paper: 'rock',
-            //     //scissors: 'paper'
-            // }
             tools: [
-                'Rock',
-                'Paper',
-                'Scissors'
+                'rock',
+                'paper',
+                'scissors'
             ],
-            results: [
-                { result: 'You win' },
-                { result: 'Computer wins' },
-                { result: 'Tie game' }  
-            ]
+            rules: { 
+                rock: 'scissors',
+                paper: 'rock',
+                scissors: 'paper'
+            },
+            message: 'Please play!',
+            userChoice: '',
+            computerChoice: ''
         }
-    },
+    },    
     methods: {
-        func(theResult) { // rename this function at some point
-            // console.log(this.results[0].result)
-            theResult = this.results[0].result
+        playGame(user) {
+            let AI = this.tools[Math.floor(Math.random() * this.tools.length)]
+
+            if (AI == this.rules[user]) {
+                this.message = 'You win'
+                this.userChoice = user
+                this.computerChoice = AI
+            } else if (user == this.rules[AI]) {
+                this.message = 'Computer wins'
+                this.userChoice = user
+                this.computerChoice = AI
+            } else if (user == AI) {
+                this.message = 'Tie game'
+                this.userChoice = user
+                this.computerChoice = AI
+            } else {
+                this.message = 'mistake'
+                this.userChoice = 'mistake'
+                this.computerChoice = 'mistake'
+            }
         }
     }
 }
 
 Vue.createApp(App).mount('#app')
-
-
-// old questions for self:
-// R my thought of storing the result string in "result" because I THINK this will render '{{ result }}' properly.
-// The key is figuring out how to handle the 'rules' object.
-// Also: Can I just have one of the three (RPS) <li> in the HTML by having an array of them in JS and looping through them in the HTML?
